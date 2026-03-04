792f86a6c60c328330ffb008faea009b95495d43
LUCYPAPA <joey8522@gmail.com>
2026-03-04 16:41:15 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ App({
   onLaunch() {
     if (wx.cloud) {
       wx.cloud.init({
-        env: 'cloud1-0gw8283g4b8815c5',
+        env: 'cloud1-0gqcenjqfe77a332',
         traceUser: true,
       })
     }
